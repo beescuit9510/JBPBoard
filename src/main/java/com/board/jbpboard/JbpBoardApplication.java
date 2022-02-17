09f294a816541b15555a1e1e4646d6f5be5b1aaa
pyojeyn <jeynpyo@gmail.com>
2022-02-17 09:42:48 +0900

    init project

---
+++ src/main/java/com/board/jbpboard/JbpBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.jbpboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbpBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbpBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/board/jbpboard/JbpBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.jbpboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbpBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
